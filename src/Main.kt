3e311a3d5bb1f1f531fa557e255794c652e12c33
Andrey <56609893+sn41@users.noreply.github.com>
2024-11-26 09:55:18 +0500

    Добавлен список list для сбора статей article в цикле чтения файла словаря, и добавлен цикл вывода списка на экран.

--- src/Main.kt
+++ src/Main.kt
@@ -2,13 +2,18 @@ import java.io.File
 
 fun main() {
     val file = File("resources/verbs100.txt")
+
+    val list = mutableListOf<Article>()
+
     file.forEachLine { line ->
         val parts = line.split("%")
         val article = Article(parts[0], parts[1])
-        println(article)
+        list.add(article)
     }
+
+    list.forEach { println(it) }
 }
 
-data class Article(val kz: String, val ru: String){
-    override fun toString()= "$kz = $ru"
+data class Article(val kz: String, val ru: String) {
+    override fun toString() = "$kz = $ru"
 }
