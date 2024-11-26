eeda5634421bb2a06b75281741e8656ebe9532c0
Andrey <56609893+sn41@users.noreply.github.com>
2024-11-26 09:51:00 +0500

    Выполнена опримизация кода

--- src/Main.kt
+++ src/Main.kt
@@ -4,64 +4,11 @@ fun main() {
     val file = File("resources/verbs100.txt")
     file.forEachLine { line ->
         val parts = line.split("%")
-        val partKz = parts[0]
-        val partRu = parts[1]
-//        println(partKz + "=" + partRu)
-
-        /* 5
-        val article = Article()
-        article.kz = partKz
-        article.ru = partRu
-        */
-
-        //5
-        val article = Article(partKz, partRu)
-
-//        println(article.kz + "=" + article.ru)
+        val article = Article(parts[0], parts[1])
         println(article)
-
-
     }
 }
 
 data class Article(val kz: String, val ru: String){
     override fun toString()= "$kz = $ru"
 }
-
-class ArticleOld {
-    // 5 var kz: String = ""
-    // 5 var ru: String = ""
-
-    //5
-    val kz: String
-    val ru: String
-    constructor(kz: String, ru: String) {
-        this.kz = kz
-        this.ru = ru
-    }
-
-
-    //article.kz + "=" + article.ru
-    /*1
-    override fun toString(): String {
-        return this.kz + "=" + this.ru
-    }
-    */
-
-    /*2
-    override fun toString(): String {
-        return kz + "=" + ru
-    }
-    */
-
-    /*3
-    override fun toString(): String {
-        return "$kz = $ru"
-    }
-    */
-
-    //4
-    override fun toString()= "$kz = $ru"
-
-}
-
