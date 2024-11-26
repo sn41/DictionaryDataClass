8a65d9fa080ed17392bb2b174ff6cb2faf5ac2f3
Andrey <56609893+sn41@users.noreply.github.com>
2024-11-26 10:09:47 +0500

    Добавлена функция println, последовательного вывода элементов списка на экран
    Добавлена readFileTo, функция-расширение для класса File, для чтения файла словаря в список статей Article
    
    1. Изменена функция main()

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,23 @@
 import java.io.File
 
 fun main() {
+    val list = ArrayList<Article>()
     val file = File("resources/verbs100.txt")
+    file.readFileTo(list)
+    println(list)
+}
 
-    val list = mutableListOf<Article>()
+fun println(list:ArrayList<Article>){
+    list.forEach { article ->
+        println(article) }
+}
 
-    file.forEachLine { line ->
+fun File.readFileTo(list:ArrayList<Article>){
+    this.forEachLine { line ->
         val parts = line.split("%")
         val article = Article(parts[0], parts[1])
         list.add(article)
     }
-
-    list.forEach { println(it) }
 }
 
 data class Article(val kz: String, val ru: String) {
