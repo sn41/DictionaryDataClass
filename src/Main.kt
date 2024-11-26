c14a8ed193ee804f411f91becfcba4ec93324d7b
Andrey <56609893+sn41@users.noreply.github.com>
2024-11-26 09:38:44 +0500

    Добавлен класс Article. Добавлена сохранение фрагментов строки в объекте класса Article.

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,16 @@ fun main() {
         val parts = line.split("%")
         val partKz = parts[0]
         val partRu = parts[1]
-        println(partKz + "=" + partRu)
+//        println(partKz + "=" + partRu)
+        val article = Article()
+        article.kz = partKz
+        article.ru = partRu
+        println(article.kz + "=" + article.ru)
+
     }
+}
+
+class Article {
+    var kz: String = ""
+    var ru: String = ""
 }
